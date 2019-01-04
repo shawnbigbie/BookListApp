63a8bab7718ca6f126e806410e17395589c94188
Shawn M Bigbie <boboargos@outlook.com>
2019-01-03 20:54:24 -0700

    update fields clear after enter

--- app.js
+++ app.js
@@ -1,134 +1,134 @@
 // Contact Class: Represents a Contact
 class Contact {
-    constructor(CName, CAddress, CNum) {
-      this.CName = CName;
-      this.CAddress = CAddress;
-      this.CNum = CNum;
+  constructor(name, address, num) {
+    this.name = name;
+    this.address = address;
+    this.num = num;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayContacts() {
+    const contacts = Store.getContacts();
+
+    contacts.forEach((contact) => UI.addContactToList(contact));
+  }
+
+  static addContactToList(contact) {
+    const list = document.querySelector('#contact-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${contact.name}</td>
+      <td>${contact.address}</td>
+      <td>${contact.num}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+
+  static deleteContact(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
     }
   }
-  
-  // UI Class: Handle UI Tasks
-  class UI {
-    static displayContacts() {
-      const contacts = Store.getContacts();
-  
-      contacts.forEach((contact) => UI.addContactToList(contact));
-    }
-  
-    static addContactToList(contact) {
-      const list = document.querySelector('#contact-list');
-  
-      const row = document.createElement('tr');
-  
-      row.innerHTML = `
-        <td>${contact.CName}</td>
-        <td>${contact.CAddress}</td>
-        <td>${contact.CNum}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-      `;
-  
-      list.appendChild(row);
-    }
-  
-    static deleteContact(el) {
-      if(el.classList.contains('delete')) {
-        el.parentElement.parentElement.remove();
-      }
-    }
-  
-    static showAlert(message, className) {
-      const div = document.createElement('div');
-      div.className = `alert alert-${className}`;
-      div.appendChild(document.createTextNode(message));
-      const container = document.querySelector('.container');
-      const form = document.querySelector('#contact-form');
-      container.insertBefore(div, form);
-  
-      // Vanish in 3 seconds
-      setTimeout(() => document.querySelector('.alert').remove(), 3000);
-    }
-  
-    static clearFields() {
-      document.querySelector('#CName').value = '';
-      document.querySelector('#CAddress').value = '';
-      document.querySelector('#CNum').value = '';
-    }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#contact-form');
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
   }
-  
-  // Store Class: Handles Storage
-  class Store {
-    static getContacts() {
-      let contacts;
-      if(localStorage.getItem('contacts') === null) {
-        contacts = [];
-      } else {
-        contacts = JSON.parse(localStorage.getItem('contacts'));
-      }
-  
-      return contacts;
-    }
-  
-    static addContact(contact) {
-      const contacts = Store.getcontacts();
-      contacts.push(contact);
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  
-    static removeContact(CNum) {
-      const contacts = Store.getContacts();
-  
-      contacts.forEach((contact, index) => {
-        if(contact.CNum === CNum) {
-          contacts.splice(index, 1);
-        }
-      });
-  
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
+
+  static clearFields() {
+    document.querySelector('#name').value = '';
+    document.querySelector('#address').value = '';
+    document.querySelector('#num').value = '';
   }
-  
-  // Event: Display Contacts
-  document.addEventListener('DOMContentLoaded', UI.displayContacts);
-  
-  // Event: Add a New Contact
-  document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
-  
-    // Get form values
-    const CName = document.querySelector('#CName').value;
-    const CAddress = document.querySelector('#CAddress').value;
-    const CNum = document.querySelector('#CNum').value;
-  
-    // Validate
-    if(CName === '' || CAddress === '' || CNum === '') {
-      UI.showAlert('Please fill in all fields', 'danger');
+}
+
+// Store Class: Handles Storage
+class Store {
+  static getContacts() {
+    let contacts;
+    if(localStorage.getItem('contacts') === null) {
+      contacts = [];
     } else {
-      // Instatiate Contact
-      const contact = new Contact(CName, CAddress, CNum);
-  
-      // Add Contact to UI
-      UI.addContactToList(contact);
-  
-      // Add Contact to store
-      Store.addContact(contact);
-  
-      // Show success message
-      UI.showAlert('Contact Added', 'success');
-  
-      // Clear fields
-      UI.clearFields();
+      contacts = JSON.parse(localStorage.getItem('contacts'));
     }
-  });
-  
-  // Event: Remove Contact
-  document.querySelector('#contact-list').addEventListener('click', (e) => {
-    // Remove Contact from UI
-    UI.deleteContact(e.target);
-  
-    // Remove Contact from store
-    Store.removeContact(e.target.parentElement.previousElementSibling.textContent);
-  
+
+    return contacts;
+  }
+
+  static addContact(contact) {
+    const contacts = Store.getContacts();
+    contacts.push(contact);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
+
+  static removeContact(num) {
+    const contacts = Store.getContacts();
+
+    contacts.forEach((contact, index) => {
+      if(contact.num === num) {
+        contacts.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
+}
+
+// Event: Display Contacts
+document.addEventListener('DOMContentLoaded', UI.displayContacts);
+
+// Event: Add a Contact
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const name = document.querySelector('#name').value;
+  const address = document.querySelector('#address').value;
+  const num = document.querySelector('#num').value;
+
+  // Validate
+  if(name === '' || address === '' || num === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Instatiate book
+    const contact = new Contact(name, address, num);
+
+    // Add Contact to UI
+    UI.addContactToList(contact);
+
+    // Add contact to store
+    Store.addContact(contact);
+
     // Show success message
-    UI.showAlert('Contact Removed', 'success');
-  });
+    UI.showAlert('Contact Added', 'success');
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
+
+// Event: Remove a Contact
+document.querySelector('#contact-list').addEventListener('click', (e) => {
+  // Remove contact from UI
+  UI.deleteContact(e.target);
+
+  // Remove contact from store
+  Store.removeContact(e.target.parentElement.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert('Contact Removed', 'success');
+});
